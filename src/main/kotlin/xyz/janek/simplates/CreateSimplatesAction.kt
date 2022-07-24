fb33907da32ce4f02ad08903d5b0688a6bb9ee1e
CrafterSvK <jakub@janek.xyz>
2022-07-25 00:07:16 +0200

    cleanup and Simplate creation extension

--- src/main/kotlin/xyz/janek/simplates/CreateSimplatesAction.kt
+++ src/main/kotlin/xyz/janek/simplates/CreateSimplatesAction.kt
@@ -2,4 +2,8 @@ package xyz.janek.simplates
 
 import com.intellij.ide.actions.CreateFileAction
 
-class CreateSimplatesAction : CreateFileAction("Simplates File", "Create simplates file", SimplatesIcons.file)
+class CreateSimplatesAction : CreateFileAction("Simplates File", "Create simplates file", SimplatesIcons.file) {
+    override fun getDefaultExtension(): String {
+        return SimplatesFileType.INSTANCE.defaultExtension
+    }
+}
--- src/main/kotlin/xyz/janek/simplates/language/psi/SimplatesTokenType.kt
+++ src/main/kotlin/xyz/janek/simplates/language/psi/SimplatesTokenType.kt
@@ -3,10 +3,4 @@ package xyz.janek.simplates.language.psi
 import com.intellij.psi.tree.IElementType
 import xyz.janek.simplates.SimplatesLanguage
 
-class SimplatesTokenType(debugName: String) : IElementType(debugName, SimplatesLanguage) {
-    init {
-        if (debugName == "CODE") {
-            //fixme: if code fragment what to do
-        }
-    }
-}
+class SimplatesTokenType(debugName: String) : IElementType(debugName, SimplatesLanguage)
