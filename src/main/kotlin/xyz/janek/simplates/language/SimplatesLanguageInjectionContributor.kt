e5fafe702e4ac63f1a685c6f6e4d596327a26e4c
Jiří Vrba <jiri@vrba.dev>
2022-08-13 14:46:07 +0200

    Working simplates language injection contributor

--- src/main/kotlin/xyz/janek/simplates/language/SimplatesLanguageInjectionContributor.kt
+++ src/main/kotlin/xyz/janek/simplates/language/SimplatesLanguageInjectionContributor.kt
@@ -7,31 +7,29 @@ import com.intellij.lang.injection.general.SimpleInjection
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.elementType
 import com.jetbrains.python.PythonLanguage
+import org.intellij.plugins.intelliLang.inject.config.BaseInjection
 import xyz.janek.simplates.language.psi.SimplatesTypes
 
 class SimplatesLanguageInjectionContributor : LanguageInjectionContributor {
-    override fun getInjection(context: PsiElement): Injection {
-        var language: Language? = null
+    override fun getInjection(context: PsiElement): Injection? {
+        // If the current PSI context is a code block inside a template segment
         if (context.elementType == SimplatesTypes.CODE_BLOCK) {
-            val parent = context.parent
+            // Check if the block has been annotated with a context type
+            if (context.parent?.elementType == SimplatesTypes.ANNOTATED_CODE_BLOCK) {
+                val annotation = context.parent.children.firstOrNull { it.elementType == SimplatesTypes.ANNOTATION } ?: throw IllegalStateException("Annotated code block element is missing an annotation children")
+                val content = annotation.children.firstOrNull { it.elementType == SimplatesTypes.CONTENT_TYPE }
 
-            if (parent != null && parent.elementType == SimplatesTypes.ANNOTATED_CODE_BLOCK) {
-                val contentType = parent.firstChild?.firstChild?.nextSibling?.nextSibling
+                // Find language matching the mime type provided by the annotation or fallback to python
+                val language = content?.firstChild
+                    ?.let { Language.findInstancesByMimeType(it.text).firstOrNull() }
+                    ?: PythonLanguage.INSTANCE
 
-                if (contentType != null && contentType.elementType == SimplatesTypes.CONTENT_TYPE) {
-                    val languages = Language.findInstancesByMimeType(contentType.text).toTypedArray()
-
-                    if (languages.isNotEmpty()) {
-                        language = languages[0]
-                    }
-                }
+                return BaseInjection("", language.id, "", "")
             }
+
+            return SimpleInjection(PythonLanguage.INSTANCE, "", "", null)
         }
 
-        if (language != null) {
-            return SimpleInjection(language, "", "", null)
-        }
-
-        return SimpleInjection(PythonLanguage.INSTANCE, "", "", null)
+        return null
     }
 }
